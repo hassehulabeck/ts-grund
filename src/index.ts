645fddd43263c3604b62dcd0b072200618a0b161
hassehulabeck <hansbudandersson@gmail.com>
2020-09-21 11:12:09 +0200

    First commit

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+let arr: number[] = [3, 44, 5];
+let x: string = "slfjadflj";
+
+let z: [Boolean, String];
+
+z = [false, "My name"];
+
+let aa: unknown;
+
+aa = 4;
+
+const y: number = 4;
+
+interface User {
+    name: string;
+    id: number;
+}
+
+const user: User = {
+    name: "3b",
+    id: 3,
+};
+
+console.log(user);
+
+enum Color {
+    Red,
+    Green,
+    Blue,
+}
+let c: Color = Color.Red;
