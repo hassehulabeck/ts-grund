f9378645ae36a6123f2495536004d7b394095c7f
hassehulabeck <hansbudandersson@gmail.com>
2020-09-22 14:03:16 +0200

    A lot of single lines

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,10 @@ interface Jersey {
     size: Size;
 }
 
+interface isRetiredFunction {
+    (jersey: Jersey): boolean;
+}
+
 enum Size {
     xs,
     s,
@@ -21,12 +25,16 @@ class PlayerJersey implements Jersey {
     playerNumber: number;
     primaryColor: string;
     size: Size;
+    secondaryColor?: string;
+    sponsorLogo?: string;
 
     constructor(jersey: Jersey) {
         this.playerName = jersey.playerName;
         this.playerNumber = jersey.playerNumber;
         this.primaryColor = jersey.primaryColor;
         this.size = jersey.size;
+        this.secondaryColor = jersey.secondaryColor;
+        this.sponsorLogo = jersey.sponsorLogo;
     }
 }
 
@@ -35,18 +43,23 @@ let myJersey: Jersey = {
     playerNumber: 52,
     primaryColor: "red",
     size: Size.xl,
+    sponsorLogo: "slfkjsdf",
 };
 
 let newJersey = new PlayerJersey(myJersey);
 
 console.log(newJersey);
 
+(window as any).newJersey = newJersey;
+
 console.log("Storlek: " + Size[myJersey.size]);
 
 let retiredNumbers: number[] = [2, 3, 52];
 
-function isNumberRetired(jersey: Jersey): boolean {
+let isNumberRetired: isRetiredFunction;
+
+isNumberRetired = function (jersey: Jersey): boolean {
     return retiredNumbers.includes(jersey.playerNumber);
-}
+};
 
 console.log(isNumberRetired(myJersey));
