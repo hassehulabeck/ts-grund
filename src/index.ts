1618d9e8b6dd78e620c1f3497a036c71e6c3909f
hassehulabeck <hansbudandersson@gmail.com>
2020-09-22 13:29:45 +0200

    Added some class

--- src/index.ts
+++ src/index.ts
@@ -16,9 +16,37 @@ enum Size {
     xxl,
 }
 
+class PlayerJersey implements Jersey {
+    playerName: string;
+    playerNumber: number;
+    primaryColor: string;
+    size: Size;
+
+    constructor(jersey: Jersey) {
+        this.playerName = jersey.playerName;
+        this.playerNumber = jersey.playerNumber;
+        this.primaryColor = jersey.primaryColor;
+        this.size = jersey.size;
+    }
+}
+
 let myJersey: Jersey = {
     playerName: "Andersson",
     playerNumber: 52,
     primaryColor: "red",
     size: Size.xl,
 };
+
+let newJersey = new PlayerJersey(myJersey);
+
+console.log(newJersey);
+
+console.log("Storlek: " + Size[myJersey.size]);
+
+let retiredNumbers: number[] = [2, 3, 52];
+
+function isNumberRetired(jersey: Jersey): boolean {
+    return retiredNumbers.includes(jersey.playerNumber);
+}
+
+console.log(isNumberRetired(myJersey));
