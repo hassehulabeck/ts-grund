a5518c0ef552daaa3bbdd438a670d1e63425d580
hassehulabeck <hansbudandersson@gmail.com>
2020-09-22 14:12:02 +0200

    Added interface for products

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,13 @@ enum Color {
     Cyan,
 }
 
+// Ett interface för products-arrayen
+interface Products {
+    [index: number]: Product;
+}
+
+let products: Products = [];
+
 interface Product {
     id: number;
     name: string;
@@ -13,7 +20,7 @@ interface Product {
     color?: Color;
 }
 
-let product = {
+let toyCar = {
     id: 3,
     name: "Toycar AE-12",
     price: 39.39,
@@ -30,3 +37,11 @@ writeProduct = function (prod: Product) {
 };
 
 console.log(writeProduct(product));
+
+function addProduct(product: Product): void {
+    products.push(product);
+}
+
+addProduct(toyCar);
+
+(window as any).products = products;
